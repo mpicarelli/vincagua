7dc0312d570e6b153b2e97894d18078332c80dd0
Maximiliano Picarelli <mpicarelli89@gmail.com>
2021-04-01 19:58:08 -0300

    agregue DOM

--- JS/reservas.js
+++ JS/reservas.js
@@ -1,7 +1,5 @@
 //VARIABLES
 
-let deptoNumero = Number(prompt("que departamento desea?"));
-let cantHuesped = Number(prompt("cuantas personas se van a hospedar"));
 
 //defino constructor de departamentos
 
@@ -13,9 +11,10 @@ class Depto {
         this.disponible = disponible;
     }
     
-capacidad(){
-    if (cantHuesped > this.personas){
-        return false
+    capacidad(){
+        let cantHuesped = Number(prompt("cuantas personas se van a hospedar"));
+        if (cantHuesped > this.personas){
+            return false
         } else {
             let diferencia = this.personas - cantHuesped;
             if (diferencia == 0){
@@ -26,7 +25,7 @@ capacidad(){
             return true
         }
     }
-    disponibilidad(){
+    /* disponibilidad(){
         if (deptoNumero == this.numero){
             if(this.disponible == false){
                 alert("Este Depto no esta disponible. Por favor elija otro");
@@ -35,7 +34,7 @@ capacidad(){
                 alert("Felicitaciones! ya tiene su reserva");
             } 
         }
-    }
+    } */
 }
 
 //objetos departamentos
@@ -47,23 +46,78 @@ departamentos.push (new Depto (4, 4, 3000, true));
 departamentos.push (new Depto (5, 5, 3500, true));
 
 
-//logica de reservas
+//RESERVAR
 
-function reservar (deptoNumero){
+function reservar (){
+    let deptoNumero = Number(prompt("que departamento desea?"));
     for (i=0; i<departamentos.length; i++){
         if(deptoNumero == departamentos[i].numero){
             if (departamentos[i].capacidad() == false){
                 alert("La cantidad de huespedes supera a la capacidad del depto. Por favor elija uno mas grande");
             } else {
-                departamentos[i].disponibilidad();
+                if (deptoNumero == departamentos[i].numero){
+                    if(departamentos[i].disponible == false){
+                        alert("Este Depto no esta disponible. Por favor elija otro");
+                    } else {
+                        departamentos[i].disponible = false;
+                        alert("Felicitaciones! ya tiene su reserva");
+                    } 
+                }
             }
         }
     }
 }
 
-reservar(deptoNumero);
+reservar();
 
+//DOM DEPARTAMENTOS
 
+departamentos.forEach( dto => {
+    let article = document.createElement('article');
+    article.setAttribute('class','col mb-4')
+    
+    let div     = document.createElement('div');
+    article.appendChild(div)
+    div.setAttribute('class','card h-100')
+    
+    let img     = document.createElement('img')
+    div.appendChild(img);
+    img.setAttribute('src','https://via.placeholder.com/300x250?text=depto1')
+    img.setAttribute('class','card-img-top')
+    
+    let divCard = document.createElement('div');
+    div.appendChild(divCard);
+    divCard.setAttribute('class','card-body')
+    
+    let h5      = document.createElement('h5');
+    divCard.appendChild(h5);
+    h5.setAttribute('class','card-title');
+    h5.textContent= 'DEPARTAMENTO';
+    
+    let ul      = document.createElement('ul'); 
+    divCard.appendChild(ul);
+    ul.setAttribute('class','card-text');
+    
+    let li1     = document.createElement('li');
+    ul.appendChild(li1);
+    li1.textContent=`Departamento ${dto.numero}`;
+    
+    let li2     = document.createElement('li');
+    ul.appendChild(li2);
+    li2.textContent=`Ocupantes: ${dto.personas}`;
+    
+    let li3     = document.createElement('li');
+    ul.appendChild(li3);
+    li3.textContent=`Precio por noche: $${dto.precio}`;
+    
+    let alink   = document.createElement('a');
+    divCard.appendChild(alink);
+    alink.setAttribute('href','#');
+    alink.setAttribute('class','btn btn-primary');
+    alink.textContent="RESERVAR"
+    
+    document.querySelector("#deptos").appendChild(article)
+})
 
 
 //Mostrar estado de los departamentos post reserva
