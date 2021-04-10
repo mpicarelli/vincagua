0b14311d60e45efaad0697956cf6943e4bb9cfd5
Maximiliano Picarelli <mpicarelli89@gmail.com>
2021-04-10 11:05:38 -0300

    ordene estilos y otras cosas

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 //VARIABLES
-let cantHuesped = Number(prompt("cuantas personas se van a hospedar"));
+//let deptoNumero = Number(prompt("que departamento desea?"));
+//let cantHuesped = Number(prompt("cuantas personas se van a hospedar"));
 
 //defino constructor de departamentos
 
@@ -39,7 +40,6 @@ localStorage.setItem("departamentos", JSON.stringify(departamentos));
 //RESERVAR
 
 function reservar (){
-    let deptoNumero = Number(prompt("que departamento desea?"));
     for (i=0; i<departamentos.length; i++){
         if(deptoNumero == departamentos[i].numero){
             if (departamentos[i].capacidad() == false){
@@ -61,51 +61,51 @@ function reservar (){
 //reservar();
 
 //DOM DEPARTAMENTOS
-const disponibles = departamentos.filter(Depto => Depto.personas >= cantHuesped);
 
-disponibles.forEach( dto => {
+
+departamentos.forEach( dto => {
     let article = document.createElement('article');
-    article.setAttribute('class','col mb-4')
+        article.setAttribute('class','col mb-4')
     
     let div     = document.createElement('div');
-    article.appendChild(div)
-    div.setAttribute('class','card h-100')
+        article.appendChild(div)
+        div.setAttribute('class','card h-100')
     
     let img     = document.createElement('img')
-    div.appendChild(img);
-    img.setAttribute('src',`https://via.placeholder.com/300x250?text=depto${dto.numero}`)
-    img.setAttribute('class','card-img-top')
+        div.appendChild(img);
+        img.setAttribute('src',`https://via.placeholder.com/300x250?text=depto${dto.numero}`)
+        img.setAttribute('class','card-img-top')
     
     let divCard = document.createElement('div');
-    div.appendChild(divCard);
-    divCard.setAttribute('class','card-body')
+        div.appendChild(divCard);
+        divCard.setAttribute('class','card-body')
     
     let h5      = document.createElement('h5');
-    divCard.appendChild(h5);
-    h5.setAttribute('class','card-title');
-    h5.textContent= 'DEPARTAMENTO';
+        divCard.appendChild(h5);
+        h5.setAttribute('class','card-title');
+        h5.textContent= 'DEPARTAMENTO';
     
     let ul      = document.createElement('ul'); 
-    divCard.appendChild(ul);
-    ul.setAttribute('class','card-text');
+        divCard.appendChild(ul);
+        ul.setAttribute('class','card-text');
     
     let li1     = document.createElement('li');
-    ul.appendChild(li1);
-    li1.textContent=`Departamento ${dto.numero}`;
+        ul.appendChild(li1);
+        li1.textContent=`Departamento ${dto.numero}`;
     
     let li2     = document.createElement('li');
-    ul.appendChild(li2);
-    li2.textContent=`Ocupantes: ${dto.personas}`;
+        ul.appendChild(li2);
+        li2.textContent=`Ocupantes: ${dto.personas}`;
     
     let li3     = document.createElement('li');
-    ul.appendChild(li3);
-    li3.textContent=`Precio por noche: $${dto.precio}`;
+        ul.appendChild(li3);
+        li3.textContent=`Precio por noche: $${dto.precio}`;
     
     let alink   = document.createElement('a');
-    divCard.appendChild(alink);
-    alink.setAttribute('href','#');
-    alink.setAttribute('class','btn btn-primary');
-    alink.textContent="RESERVAR"
+        divCard.appendChild(alink);
+        alink.setAttribute('href','#');
+        alink.setAttribute('class','btn btn-primary');
+        alink.textContent="RESERVAR"
     
     document.querySelector("#deptos").appendChild(article)
 })
